b63fd7bfb52042624821b95231700eea4dd4b3d1
Umberto Nicoletti <umberto.nicoletti@gmail.com>
2024-05-06 08:31:02 +0000

    basic go program structure

---
+++ go.mod
@@ -0,0 +1,16 @@
+module playground
+
+go 1.22.2
+
+require (
+	github.com/bndr/gojenkins v1.1.0
+	gorm.io/driver/sqlite v1.5.5
+	gorm.io/gorm v1.25.10
+)
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-sqlite3 v1.14.17 // indirect
+	golang.org/x/net v0.0.0-20200114155413-6afb5195e5aa // indirect
+)
---
+++ jenkins_stats/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/bndr/gojenkins"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type Build struct {
+	gorm.Model
+	Id     uint16
+	Status string
+}
+
+func main() {
+	db, err := gorm.Open(sqlite.Open("/tmp/jenkins.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// Migrate the schema
+	db.AutoMigrate(&Build{})
+
+	ctx := context.Background()
+	jenkins := gojenkins.CreateJenkins(nil, "https://jenkins.ad1.proemion.com/", os.Getenv("JENKINS_USER"), os.Getenv("JENKINS_PASSWORD"))
+	_, err = jenkins.Init(ctx)
+	if err != nil {
+		panic(fmt.Sprintln("failed to connect to jenkins:", err))
+	}
+
+	db.Create(&Build{Id: 1, Status: "success"})
+
+	var build Build
+	db.First(&build, 1)
+	db.First(&build, "id = ?", 1)
+}
