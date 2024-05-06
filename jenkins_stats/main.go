48fd7dd13c55db934cf51f9fe7e2d9e34a310886
Umberto Nicoletti <umberto.nicoletti@gmail.com>
2024-05-06 19:43:11 +0000

    cleanup

--- jenkins_stats/main.go
+++ jenkins_stats/main.go
@@ -19,6 +19,15 @@ type Build struct {
 	Duration int64
 }
 
+func initGormDB() *gorm.DB {
+	db, err := gorm.Open(sqlite.Open("/tmp/jenkins.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	db.AutoMigrate(&Build{})
+	return db
+}
+
 func save(db gorm.DB, build gojenkins.Build, jobName string) {
 	var found Build
 	db.First(&found, "build_id=? and job_name=?", build.Raw.ID, jobName)
@@ -39,41 +48,29 @@ func save(db gorm.DB, build gojenkins.Build, jobName string) {
 }
 
 func main() {
-	db, err := gorm.Open(sqlite.Open("/tmp/jenkins.db"), &gorm.Config{})
-	if err != nil {
-		panic("failed to connect database")
-	}
-
-	// Migrate the schema
-	db.AutoMigrate(&Build{})
-
 	ctx := context.Background()
 	jenkins := gojenkins.CreateJenkins(nil, os.Getenv("JENKINS_URL"), os.Getenv("JENKINS_USER"), os.Getenv("JENKINS_PASSWORD"))
-	_, err = jenkins.Init(ctx)
+	_, err := jenkins.Init(ctx)
 	if err != nil {
 		panic(fmt.Sprintln("failed to connect to jenkins:", err))
 	}
-
+	db := initGormDB()
 	if len(os.Args) == 1 {
 		fmt.Println("Supply job name as first and only argument")
 	} else {
 		jobName := os.Args[1]
-		jobs, err := jenkins.GetAllBuildIds(ctx, jobName)
+		ids, err := jenkins.GetAllBuildIds(ctx, jobName)
 		if err != nil {
 			panic(fmt.Sprintln("cannot retrieve builds for job", jobName, ":", err))
 		}
-
 		count := 0
-		for _, job := range jobs {
+		for _, id := range ids {
 			count++
-			buildId := job.Number
-			build, err := jenkins.GetBuild(ctx, jobName, buildId)
-
+			build, err := jenkins.GetBuild(ctx, jobName, id.Number)
 			if err != nil {
-				panic(fmt.Sprintln("cannot retrieve build", buildId, " for job", jobName, ":", err))
+				panic(fmt.Sprintln("cannot retrieve build", id.Number, " for job", jobName, ":", err))
 			}
-
-			fmt.Printf("Build [%s/%d]: %s [%s]\n", jobName, buildId, build.GetResult(), time.UnixMilli(build.Raw.Timestamp).Format(time.RFC1123Z))
+			fmt.Printf("Build [%s/%d]: %s [%s]\n", jobName, id.Number, build.GetResult(), time.UnixMilli(build.Raw.Timestamp).Format(time.RFC1123Z))
 			save(*db, *build, jobName)
 		}
 		fmt.Println("found", count, "builds for", jobName)
